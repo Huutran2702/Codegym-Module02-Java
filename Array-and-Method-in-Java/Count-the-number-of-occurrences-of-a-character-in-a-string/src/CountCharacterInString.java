40f16dc895d7e44ba97d3cd81cc9dd9f3d6a0b83
Hữu Trần Ngọc <ngochuutran2702@gmail.com>
2021-10-11 12:00:36 +0700

    Đếm số lần xuất hiện của ký tự trong chuỗi

---
+++ Array-and-Method-in-Java/Count-the-number-of-occurrences-of-a-character-in-a-string/src/CountCharacterInString.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class CountCharacterInString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "Hello world!";
+        System.out.println("Enter character you want to count: ");
+        String input = sc.nextLine();
+        int count = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if (input.equalsIgnoreCase(String.valueOf(str.charAt(i)))) {
+                count +=1;
+            }
+        }
+        System.out.println("The number of occurrences of character "+input+" in a string is: " +count);
+    }
+}
