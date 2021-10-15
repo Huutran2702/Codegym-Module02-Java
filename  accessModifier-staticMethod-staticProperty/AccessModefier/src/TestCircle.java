fcc792f5b37b6d146861a0db846ec64fc1f9b341
Hữu Trần Ngọc <ngochuutran2702@gmail.com>
2021-10-15 15:12:48 +0700

    change

---  accessModifier-staticMethod-staticProperty/AccessModefier/src/TestCircle.java
+++
@@ -1,8 +0,0 @@
-public class TestCircle {
-    public static void main(String[] args) {
-        Circle circle1 = new Circle(10);
-        double radius1 = circle1.getRadius();
-        System.out.println(radius1);
-        System.out.println(circle1.getArea());
-    }
-}
---  accessModifier-staticMethod-staticProperty/Build-write-only-classes-in-Java/src/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        Student student = new Student();
-        student.setName("Huu Pro");
-        student.setClasses("C0821G1");
-        System.out.println(student);
-    }
-}
