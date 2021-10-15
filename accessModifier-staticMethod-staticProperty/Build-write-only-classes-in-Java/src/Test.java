498f43c82c3f15acdf18987191d2331373daa074
Hữu Trần Ngọc <ngochuutran2702@gmail.com>
2021-10-15 16:17:44 +0700

    Build-write-only-classes-in-Java

---
+++ accessModifier-staticMethod-staticProperty/Build-write-only-classes-in-Java/src/Test.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Huu Pro");
+        student.setClasses("C0821G1");
+        System.out.println(student);
+    }
+}
