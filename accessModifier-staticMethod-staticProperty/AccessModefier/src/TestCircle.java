e528ab16c87b797067186b6d186329d9f252e51e
Hữu Trần Ngọc <ngochuutran2702@gmail.com>
2021-10-15 16:10:55 +0700

    Access modifier

---
+++ accessModifier-staticMethod-staticProperty/AccessModefier/src/TestCircle.java
@@ -0,0 +1,8 @@
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle1 = new Circle(10);
+        double radius1 = circle1.getRadius();
+        System.out.println(radius1);
+        System.out.println(circle1.getArea());
+    }
+}
